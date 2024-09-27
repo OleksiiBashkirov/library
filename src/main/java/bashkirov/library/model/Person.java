27ddecfce4f202cbb48b2d4ca8862a5b0218ebb5
Oleksii Bashkirov <bashkirov.o.u@ukr.net>
2024-09-27 12:22:21 +0300

    - created project; - added model Person;

---
+++ src/main/java/bashkirov/library/model/Person.java
@@ -0,0 +1,30 @@
+package bashkirov.library.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+    private int id;
+
+    @NotBlank(message = "Field cannot be empty")
+    @Size(min = 2, max = 32, message = "Field cannot be less 2 and more 32 characters")
+    private String name;
+
+    @NotBlank(message = "Field cannot be empty")
+    @Size(min = 1, max = 32, message = "Field cannot be less 1 and more 32 characters")
+    private String lastname;
+
+    @NotBlank(message = "Field cannot be empty")
+    @Pattern(regexp = "^[0-9]{10}$")
+    private String inn;
+
+    @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$")
+    private String birthdate;
+}
