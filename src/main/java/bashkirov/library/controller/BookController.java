d2deb8100f8bf70529f99b2f6e4362b6d9a71c0f
Oleksii Bashkirov <bashkirov.o.u@ukr.net>
2024-09-28 06:50:37 +0300

    - у BookController.java додано метод 'deleteById' - додано кнопку 'Delete' у форму book-page.html

--- src/main/java/bashkirov/library/controller/BookController.java
+++ src/main/java/bashkirov/library/controller/BookController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -82,5 +83,11 @@ public class BookController {
         return "redirect:/book/" + bookId;
     }
 
-
+    @DeleteMapping("/{id}")
+    public String deleteById(
+            @PathVariable("id") int bookId
+    ) {
+        bookDao.deleteById(bookId);
+        return "redirect:/book";
+    }
 }
